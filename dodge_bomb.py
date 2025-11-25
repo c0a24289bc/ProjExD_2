7e8b855227c02db20cdd2834a4a285e04430d1b1
Daisuke <c0a24289bc@edu.teu.ac.jp>
2025-11-25 14:41:27 +0900

    練習３：はみ出し判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,19 @@ DELTA = {
 }#練習1
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct : pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：判定結果タプル（横方向、縦方向）
+    画面内ならTrue, 画面外ならFalse
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -53,7 +66,14 @@ def main():
                 sum_mv[1] += mv[1]
 
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True): # 画面外なら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) # 移動をなかったことにする
         screen.blit(kk_img, kk_rct)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko: # 横方向にはみ出していたら
+            vx *= -1
+        if not tate: # 縦方向にはみ出していたら
+            vy *= -1
         bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
